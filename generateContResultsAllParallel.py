13ea5330f892782505ceb2c0cfc7ca4b5112a08b
TD@arui <duhantanishq@gmail.com>
2025-11-27 15:35:41 +0800

    remove pycache

--- generateContResultsAllParallel.py
+++ generateContResultsAllParallel.py
@@ -6,6 +6,7 @@ import os
 import glob
 
 folder_path = 'Results/ContRawData'
+
 # Get a list of all .dat files in the folder
 dat_files = glob.glob(os.path.join(folder_path, '*.dat'))
 
@@ -13,6 +14,7 @@ dat_files = glob.glob(os.path.join(folder_path, '*.dat'))
 for file in dat_files:
     os.remove(file)
     print(f"Deleted {file}")
+
 
 listOfMethods = [OriginalADG, SAGE, MAGE]
 
@@ -28,17 +30,16 @@ def getOneEpData(NUM_AGENTS, FPS, index):
     timeStore = np.zeros(len(listOfMethods))
     commsStore = np.zeros(len(listOfMethods))
     
-    # Fname = "tanishq_coin/paths/agents"+str(NUM_AGENTS)+"_fps"+str(FPS)
-    Fname = "{}Agents_{}fps".format(NUM_AGENTS, FPS)
+    Fname = "scenario/paths/agents"+str(NUM_AGENTS)+"_fps"+str(FPS)
 
-    with open(Fname+'{}.json'.format(index), 'r') as f:
+    with open(Fname+'/{}.json'.format(index), 'r') as f:
         try:
             data = json.load(f)
         except:
             print(index)
             return None
     allPos=jsonToNpy(data,NUM_AGENTS)
-    
+
     for idx, val in enumerate(listOfMethods):
         commsLen, timeTaken = testTime(val, allPos, filepath+"_"+str(index)+".dat")
         timeStore[idx] = timeTaken    
@@ -47,13 +48,14 @@ def getOneEpData(NUM_AGENTS, FPS, index):
     with open(filepath+".csv", "+a") as f:
             writer = csv.writer(f)
             writer.writerows([[NUM_AGENTS, FPS, index, allPos.shape[1], "|", timeStore[0], "|", commsStore[0]]])
+
     try:
         os.remove(filepath+"_"+str(index)+".dat")
     except:
         pass
 
 ray.init()
-pairsLeft = [(10,10)]
+pairsLeft = [(10,10), (10,20), (10,25), (10,50), (10,100), (20,10), (20,20), (20,25), (20,50)]
 allPairs = []
 for (NUM_AGENTS, FPS) in pairsLeft:
     filepath = "Results/ContRawData/"+str(NUM_AGENTS)+"_MAGE+"+str(FPS)+".csv"
@@ -68,11 +70,10 @@ for (NUM_AGENTS, FPS) in pairsLeft:
             except ValueError:
                 continue
 
-    missing_numbers = [num for num in range(2) if num not in existing_numbers]
+    missing_numbers = [num for num in range(100) if num not in existing_numbers]
     
     for num in missing_numbers:
         allPairs.append((NUM_AGENTS, FPS, num))
-        
 np.random.shuffle(allPairs)
 ray.get([getOneEpData.remote(*allPairs[i]) for i in range(len(allPairs))])
 print("FINISHED")
